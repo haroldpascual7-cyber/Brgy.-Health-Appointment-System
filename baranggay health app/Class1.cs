ea62df88a9604b28dc96ba45bccac7cf61850f10
Harold Pascual <haroldpascual7@gmail.com>
2025-09-18 11:39:15 +0800

    add new class

---
+++ baranggay health app/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baranggay_health_app
+{
+    class Class1
+    {
+    }
+}
